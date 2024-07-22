02acc3932e99b6c33000a1958d6e216757642b1b
Kishan Gupta <166640168+Kishan-Guptaa@users.noreply.github.com>
2024-07-22 20:12:23 +0530

    DAY_06_Complete!

---
+++ Day_06_Array/index7.js
@@ -0,0 +1,23 @@
+//DAY:07 -OBJECTs
+
+//Activity 1: Object Creation and Access
+
+//Task 1: Create an object representing a book with properties like title, author, and year, and log the object to the console.
+const The_Power_of_your_subconscious_mind={
+    author:"Dr Joseph Murphy",
+    title:" a self-help book that explores the potential and influence the subconscious mind can have on our lives",
+    year:1963,
+}
+
+console.log(The_Power_of_your_subconscious_mind.author);
+console.log(The_Power_of_your_subconscious_mind.title);
+console.log(The_Power_of_your_subconscious_mind.year);
+
+//Task 2: Access and log the title and author properties of the book object.
+console.log(The_Power_of_your_subconscious_mind.title);
+console.log(The_Power_of_your_subconscious_mind.year);
+
+
+//Activity 2: Object Methods
+
+//Task 3: Add a method to the book object that returns a string with the book's title and author, and log the result of calling this method.
